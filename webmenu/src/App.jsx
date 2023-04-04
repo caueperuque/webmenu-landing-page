c6ea6d830084a48906fe5b8972bb7beade147b2f
caue peruque <caue19052003@gmail.com>
2023-04-04 15:36:00 -0300

    teste

--- webmenu/src/App.jsx
+++ webmenu/src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
-import Form from './components/form/Form';
+// import Form from './components/form/Form';
 import Header from './components/header/Header';
 
 function App() {
   return (
     <>
       <Header></Header>
-      <Form />
     </>
   );
 }
